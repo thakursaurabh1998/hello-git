fba8fd4c126b2632a866a14d70440f52bd6d6fbe
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-06-12 21:17:39 +0530

    Jun11-r

--- Jun11/Q4.java
+++ Jun11/Q4.java
@@ -15,6 +15,17 @@ class Sorting{
             }
         }
     }
+    public void swap(int[] arr){
+        int n = arr.length;
+        int temp;
+        if(n%2!=0)
+            n--;
+        for(int i=0;i<n;i+=2){
+            temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
 }
 
 public class Q4{
@@ -26,6 +37,7 @@ public class Q4{
             arr[i] = in.nextInt();
         Sorting obj = new Sorting();
         obj.insertionSort(arr);
+        obj.swap(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
