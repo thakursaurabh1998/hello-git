3b67a67d5e172627066d140b21b792c2544fa191
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-06-29 15:33:01 +0530

    Jun28

---
+++ Jun28/app/src/main/java/com/example/thakursaurabh/jun28/FinalActitvity.java
@@ -0,0 +1,13 @@
+package com.example.thakursaurabh.jun28;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalActitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_actitvity);
+    }
+}
